6080e257b8f07a4cff9765971e62877361758779
santhoshandra <andra20_ug@ece.nits.ac.in>
2023-09-23 09:52:56 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavour");
+require("./7-mind-grenade");
+console.log(data);
+sayHi("Santhosh");
+sayHi(names.john);
+sayHi(names.peter);
